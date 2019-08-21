03ff2f84f1e32d68328d76c2dcdbc943e965bb18
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-08-21 08:26:57 -0400

    Update app.js
    update of script to change page color and automatically refresh apge which changes quote.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const   quoteBtn = document.getElementById("loadQuote"),
         bgColor = document.getElementsByTagName("body")[0],
         colorsArray = [
-            "", // empty string because the random # returned will never be 0 [unless the + 1] is removed and thus the color at position 0 will never be selected
+            "", // empty string because the random # returned will never be 0 [unless the + 1 is removed] and thus the color at position 0 will never be selected
             "pink",
             "blue",
             "black",
@@ -13,20 +13,17 @@ const   quoteBtn = document.getElementById("loadQuote"),
             "yellow",
         ];
 
-const   changeColor = function (){
+const   reloadPage = function (){
+            window.location.reload(true);
+},
+        changeColor = function (){
             let randomNum = Math.floor(Math.random() * 8)+ 1;
             bgColor.style.backgroundColor = colorsArray[randomNum];
-            console.log(randomNum); //for testing purposes        
         };
 
-//Reloads or refreshes the page which will also load a new quote, after 20 seconds
-function reloadPage (){
-    window.location.reload(true);
-    changeColor();
-}
 
 //Will calll the changeColor function definition if the show another Quote button is clicked 
 quoteBtn.addEventListener("click", changeColor);
 
 // Calls the reloadPage function every 20 seconds
-setTimeout(reloadPage, 20000);
+setInterval(reloadPage, 5000);
